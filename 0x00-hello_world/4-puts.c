f2ce0c3a809b131fb6b6c259265954f9c9d8ee31
Ahmed Mansour <ahmedkamel120012@gmail.com>
2023-02-18 05:22:41 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-/* *Write a C program that prints exactly 
- * "Programming is like building a multilingual puzzle, followed by a new line.
- * Use the function puts
+/**
+ *Write a C program that prints exactly 
+ *"Programming is like building a multilingual puzzle, followed by a new line.
+ *Use the function puts
 */
 int main(void)
 {
-	char mystr[] = "Programming is like building a multilingual puzzle\n";
-
+	char mystr[] = "\"Programming is like building a multilingual puzzle\n";
 	puts(mystr);
 	return (0);
 }
