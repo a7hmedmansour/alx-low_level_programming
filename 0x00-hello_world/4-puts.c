c701e020b7da228ed45fac9a5867dbe0aafd61f4
Ahmed Mansour <ahmedkamel120012@gmail.com>
2023-02-18 05:42:37 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
 */
 int main(void)
 {
-	char mystr[] = "\"Programming is like building a multilingual puzzle\n";
-	puts(mystr);
-	return (0);
+char mystr[] = "\"Programming is like building a multilingual puzzle";
+puts(mystr);
+return (0);
 }
