4ff9d82667ff56904ae476708aeecbfb9c1b3f91
Ahmed Mansour <ahmedkamel120012@gmail.com>
2023-02-18 08:46:44 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+/*
+ * file 5-printf.c
+ * Auth :ahmed mansour
+ *
+ */
+#include <stdio.h>
+/**
+ * main - prints exactly with proper grammar, but the outcome is a piece of art
+ *
+ *
+ * Return:return 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
