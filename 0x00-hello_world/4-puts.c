fa4a1bb3f5b2a6e7428bb5655a1ed69a475a8d81
Ahmed Mansour <ahmedkamel120012@gmail.com>
2023-02-18 05:17:46 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-int main() {
-	char mystr[]="Programming is like building a multilingual puzzle\n";
+/* *Write a C program that prints exactly 
+ * "Programming is like building a multilingual puzzle, followed by a new line.
+ * Use the function puts
+*/
+int main(void)
+{
+	char mystr[] = "Programming is like building a multilingual puzzle\n";
+
 	puts(mystr);
-	return 0;
+	return (0);
 }
