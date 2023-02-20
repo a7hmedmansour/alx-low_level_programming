2b53ea208f26ab3453ea8511faa816163688cb31
Ahmed Mansour <ahmedkamel120012@gmail.com>
2023-02-20 12:15:29 -0800

    print without any functions listed in the NAME section of the man (3) printf or man (3) puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<unistd.h>
+#include<string.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+*main- print without any functions listed in the NAME section of the man(3)
+*
+*
+*Return: 1
+*/
+int main(void)
+{
+char str []="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+int len=strlen(str);
+
+write (1,str,len);
+
+return (1);
+}
