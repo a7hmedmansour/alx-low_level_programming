9ac385a7bb05ff7b382af402b7d19c06eeb1a9ff
Ahmed Mansour <ahmedkamel120012@gmail.com>
2023-02-24 07:43:35 -0800

    The “Fizz-Buzz test”

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+*main - Fizz-Buzz test
+*for multiples of three print Fizz instead of the number
+*for the multiples of five print Buzz
+*For numbers which are multiples of both three and five print FizzBuzz
+*
+*Return: 0
+*/
+int main(void)
+{
+int count;
+for (count = 1 ; count <= 100; count++)
+{
+if (count % 3 == 0 && count % 5 == 0)
+printf("Fizz Buzz ");
+else if (count % 3 == 0)
+printf("Fizz ");
+else if (count % 5 == 0)
+printf("Buzz ");
+else
+printf("%d ", count);
+}
+printf("\n");
+return (0);
+}
