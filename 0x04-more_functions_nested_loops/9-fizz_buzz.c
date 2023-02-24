416b2e730e95ff7bbd1025438eaf7faaefff7fc5
Ahmed Mansour kamal <114818501+a7hmedmansour@users.noreply.github.com>
2023-02-24 17:54:15 +0200

    The “Fizz-Buzz test”

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,7 +13,7 @@ int count;
 for (count = 1 ; count <= 100; count++)
 {
 if (count % 3 == 0 && count % 5 == 0)
-printf("Fizz Buzz");
+printf("FizzBuzz");
 else if (count % 3 == 0)
 printf("Fizz");
 else if (count % 5 == 0)
