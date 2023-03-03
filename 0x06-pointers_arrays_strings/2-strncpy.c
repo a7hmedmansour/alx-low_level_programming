e538878c7cfae410643694e45a4444b9ae2dbfa4
Ahmed Mansour <ahmedkamel120012@gmail.com>
2023-03-03 11:24:02 -0800

    2. strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,11 +11,13 @@ char *_strncpy(char *dest, char *src, int n)
 {
 int l_src;
 int i;
+/*l_dest = strlen(dest); l_dest, */
 l_src = strlen(src);
 if (n > l_src)
 {
-for (i = 0 ; i < l_src ; i++)
+for (i = 0; i < l_src; i++)
 dest[i] = src[i];
+for (i = l_src; i < n; i++)
 dest[i] = '\0';
 }
 else
