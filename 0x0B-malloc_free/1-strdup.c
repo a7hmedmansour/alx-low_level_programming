3da1a5af537efd837efcae9340449a4c7fb64650
Ahmed Mansour <ahmedkamel120012@gmail.com>
2023-03-15 03:55:37 +0200

    a function copy the string given as a parameter.

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+/**
+*_strdup- a function copy of the string given as a parameter.
+*@str:the string given
+*Return: returns a pointer to a newly allocated space in memory
+*/
+char *_strdup(char *str)
+{
+int size = strlen(str), i;
+if (size == 0)
+return (NULL);
+else
+{
+char *dupstr;
+dupstr = malloc(sizeof(char) * size);
+if (dupstr == NULL)
+return (NULL);
+else
+{
+for (i = 0 ; str[i] != '\0'; i++)
+*(dupstr + i) = str[i];
+return (dupstr);
+}
+}
+}
