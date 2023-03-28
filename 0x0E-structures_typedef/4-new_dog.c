2d4a4d3471d21f6a1d4aa9969a9968aaf17e1de2
Ahmed Mansour <ahmedkamel120012@gmail.com>
2023-03-28 22:50:08 +0200

    a function that frees dogs.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,8 +11,9 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *N_dog;
-unsigned int i = 0, j = 0;
+
 int len, len_;
+
 len = strlen(name);
 len_ = strlen(owner);
 
@@ -22,25 +23,21 @@ if (N_dog == NULL)
 return (NULL);
 
 N_dog->name = malloc(sizeof(char) * (len + 1));
-while (name[i] != '\0' && N_dog->name)
-{
-N_dog->name[i] = name[i];
-i++;
-}
 N_dog->owner = malloc(sizeof(char) * (len_ + 1));
-while (owner[j] != '\0' && N_dog->owner)
+
+if (N_dog->name == NULL)
 {
-N_dog->owner[j] = owner[j];
-j++;
+free(N_dog);
+return (NULL);
 }
-if (!N_dog->name || !N_dog->owner)
+if (N_dog->owner == NULL)
 {
 free(N_dog);
 free(N_dog->name);
-free(N_dog->owner);
 return (NULL);
 }
+strcpy(N_dog->name, name);
+strcpy(N_dog->owner, owner);
 N_dog->age = age;
-
 return (N_dog);
 }
